2f304e64680c77a99a19a31b5bcab9e8e38e1d79
Ashutosh Nanda <ashutosh.nanda@gmail.com>
2014-08-31 23:14:25 -0400

    Changed log-cluster.py to export types.csv as well

--- log-cluster.py
+++ log-cluster.py
@@ -1,13 +1,18 @@
-#! /cygdrive/c/Python33/python.exe
+#! /usr/bin/python3
+
+import os
+import numpy as np
+from scipy.cluster.vq import kmeans, whiten, kmeans2
 
 logFileName = "trimmed-mongodb.log"
 
 logTypeInfo = "Log Types"
 logLinesInfo = "Log Lines"
 
-import os
-import numpy as np
-from scipy.cluster.vq import kmeans, whiten, kmeans2
+similarity_cutoff = 0.8
+
+csv_log_export_filename = "../log-visualization/data/log.csv"
+csv_types_export_filename = "../log-visualization/data/types.csv"
 
 ##Analysis methods
 
@@ -23,6 +28,16 @@ def prechecks():
 		print("Exiting..")
 		exit()
 
+	if os.path.isfile(csv_log_export_filename):
+		print("Found log.csv file at \"%s\".." % csv_log_export_filename)
+		print("Removing \"%s\".." % csv_log_export_filename)
+		os.remove(csv_log_export_filename)
+
+	if os.path.isfile(csv_types_export_filename):
+		print("Found types.csv file at \"%s\".." % csv_types_export_filename)
+		print("Removing \"%s\".." % csv_types_export_filename)
+		os.remove(csv_types_export_filename)
+		
 	print("Done doing prechecks..\n")
 
 def readLog():
@@ -55,6 +70,14 @@ def extract_type(logLines):
 
 	print("Done extracting type of log lines..\n")
 	return logTypes
+
+def extract_time(logLines):
+	print("Extracting timestamps of the log lines..")
+	
+	logTimes = [logLine.split(" [")[0] for logLine in logLines]
+
+	print("Done extracting timestamps of the log lines..")	
+	return logTimes
 		
 def analyze_type(logTypes):
 	print("Analyzing types of log lines..")
@@ -84,7 +107,7 @@ def analyze_type(logTypes):
 def sanitize_log_contents(logLines):
 	#Removes certain characters and isolates log contents
 	print("Sanitizing log lines..")
-	sanitizedLogContents = [logLine.split("] ")[1].strip().replace("(", "").replace(")", "").replace(":", "").replace("[", "").replace("]", "")\
+	sanitizedLogContents = [logLine.split("] ")[1].strip().replace("(", "").replace(")", " ").replace(":", " ").replace("[", "").replace("]", "")\
 				.replace("{", "").replace("}", "").replace(".", "").replace(",", "").replace("\"", "").replace("_", "") for logLine in logLines]
 	print("Done sanitizing log lines..\n")
 	return sanitizedLogContents
@@ -121,7 +144,7 @@ def analyze_corpus(words, log):
 
 def convert_log_lines(words, log):
 	print("Converting log lines into vectors..")
-	vectorLines = [[sum([word == logWord for logWord in logLine.split(" ")]) for word in words] for logLine in log]
+	vectorLines = [[1 if sum([word == logWord for logWord in logLine.split(" ")]) > 0 else 0 for word in words] for logLine in log]
 	print("Done converting log lines into vectors..\n")
 	return vectorLines
 
@@ -130,15 +153,92 @@ def clustering(vecs):
 	print("Number of lines: %d" % len(vecs))
 	print("Number of features: %d" % len(vecs[0]))
 	
-	matrix = np.asarray(vecs) #Each row represents a different line from the log file; each column represents a different feature
-	whitematrix = whiten(matrix)
-	centroids = [1, 4, 10, 20]
-	for centroid in centroids:
-		code, dist = kmeans(whitematrix, centroid)
-		print("%d --> %f" % (centroid, dist))
+	uniqueVecs = [item for (n, item) in enumerate(vecs) if item not in vecs[:n]] 
+	print("Number of Unique Vectors = %d.." % len(uniqueVecs))
+	counts = [sum([uniqueVec == vec for vec in vecs]) for uniqueVec in uniqueVecs]
+	for (i, uniqueVec) in enumerate(uniqueVecs):
+		print("Counts: %d; Max Value: %d" % (counts[i], max(uniqueVec)))
+	
+	before = len(uniqueVecs)
+	uniqueVecs = list(filter(lambda x: sum(x) != 0, uniqueVecs))
+	after = len(uniqueVecs)
+	print("Removed %d zero vecs.." % (before - after)) if before - after != 0 else None
+
+	similarities = [[dot(other, uniqueVec) / (dot(uniqueVec) * dot(other)) ** 0.5 if other != uniqueVec else 0 for other in uniqueVecs] for uniqueVec in uniqueVecs]
+	head(similarities, len(similarities))
+	nonzerosimilarities = []
+	for i in range(len(similarities)):
+		for j in range(len(similarities[i])):
+			if similarities[i][j] > 0 and similarities[i][j] not in [item[0] for item in nonzerosimilarities]:
+				nonzerosimilarities.append((similarities[i][j], i, j))
+	nonzerosimilarities.sort(key = lambda x: x[0], reverse = True)
+	print(nonzerosimilarities)
+
+	mapping = list(range(len(uniqueVecs)))
+	for item in nonzerosimilarities:
+		if item[0] > similarity_cutoff:
+			count = 0
+			first_id = min(item[1:])
+			second_id = max(item[1:])
+			mapping[first_id] = second_id
+			print("Mapped %d to %d (count = %d).." % (first_id, second_id, counts[first_id]))
+			counts[second_id] += counts[first_id]
+			counts[first_id] = 0
+	print(counts)
+
+	#matrix = np.asarray(vecs) #Each row represents a different line from the log file; each column represents a different feature
+	#whitematrix = whiten(matrix)
+	#centroids = [1, 4, 10, 20]
+	#for centroid in centroids:
+	#	code, dist = kmeans(whitematrix, centroid)
+	#	print("%d --> %f" % (centroid, dist))
+
+	return (uniqueVecs, mapping)
 	
 	print("Done clustering the logs..\n")
 
+def classify(log, logVectors, templates, mapping):
+	print("Classifying the log lines..")
+	
+	print(mapping)
+
+	for i, logline in enumerate(log):
+		index = templates.index(logVectors[i]) if logVectors[i] in templates else -1
+		print("%d: %s" % (mapping[index], logline))
+	
+
+	print("Done classifying the log lines..\n")
+
+def csv_export(log, sanitizedlog, logvectors, vectortypes, vectortimes, templates, mapping, dictionary):
+	print("Exporting data to .csv format..")
+	
+	csv_export = open(csv_log_export_filename, 'w')
+
+	csv_export.write("Timestamp;Log Type;Unique Vector ID;Matched ID;Raw Log;Sanitized Log\n")
+
+	for i in range(len(log)):
+		uniqueID = templates.index(logvectors[i])
+		csv_export.write("%s;%s;%d;%d;\"%s\";\"%s\"\n" % (vectortimes[i], vectortypes[i], uniqueID, mapping[uniqueID], log[i], sanitizedlog[i]))
+
+	csv_export.close()
+
+	csv_export = open(csv_types_export_filename, 'w')
+
+	csv_export.write("Type;Words\n")
+
+	for i, template in enumerate(templates):
+		csv_export.write("%d;\"" % i)
+		nonzero = [j for (j, value) in enumerate(template) if value != 0]
+		for j in range(len(nonzero)):
+			csv_export.write(dictionary[nonzero[j]])
+			if j != len(nonzero) - 1:
+				csv_export.write(" ")
+		csv_export.write("\"\n")
+
+	csv_export.close()
+
+	print("Done exporting data to .csv format..")
+	
 ##Utilities
 
 def noNumbers(string):
@@ -158,7 +258,13 @@ def head(lines, n, info = ""):
 	outro = "Done printing first %d lines" % n
 	if info != "":
 		outro += " of %s" % info
-	print("%s..\n" % outro)
+	print("%s.." % outro)
+
+def dot(u, v = None):
+	if not v:
+		v = u
+	assert(len(u) == len(v))
+	return sum([u[i] * v[i] for i in range(len(u))])
 
 ## Analysis Modes
 
@@ -169,12 +275,15 @@ def mainAnalysis():
 	log = preprocessLog(log)
 	logTypes = extract_type(log)
 	analyze_type(logTypes)
+	logTimes = extract_time(log)
 	sanitizedLog = sanitize_log_contents(log)
 	corpus = build_corpus(sanitizedLog)
 	analyze_corpus(corpus, sanitizedLog)
 	vectorLines = convert_log_lines(corpus, sanitizedLog)
-	clustering(vectorLines)
-	print("Exiting..")
+	(uniqueVectors, mapping) = clustering(vectorLines)
+	classify(log, vectorLines, uniqueVectors, mapping)
+	csv_export(log, sanitizedLog, vectorLines, logTypes, logTimes, uniqueVectors, mapping, corpus)
+	print("Exiting..", end = "")
 
 ## Main
 
